bbc23780300616a5c2a35c896aec6355d0dfad25
cdjianz <cdjianz@cn.ibm.com>
2019-01-10 16:15:37 +0800

    refresh some files

--- src/main/java/com/blog/BlogServiceBlogApplication.java
+++ src/main/java/com/blog/BlogServiceBlogApplication.java
@@ -3,7 +3,6 @@ package com.blog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class BlogServiceBlogApplication {
 
